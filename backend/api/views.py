d590c302cce24d6700c390e71299e5472a44e820
erenanbar <116219331+erenanbar1@users.noreply.github.com>
2025-05-25 15:25:54 +0300

    c

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 import sys
 import os
 
@@ -11,18 +9,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../sql_scripts')))
 from login import login as login_func
 from registration import register as register_func
-=======
 
-from "../sql_scripts/login.py" import login as login_func
->>>>>>> Stashed changes
-=======
-
-from "../sql_scripts/login.py" import login as login_func
->>>>>>> Stashed changes
-=======
-
-from "../sql_scripts/login.py" import login as login_func
->>>>>>> Stashed changes
 
 @csrf_exempt
 def user_login(request):
@@ -34,29 +21,17 @@ def user_login(request):
             if not email or not password:
                 return JsonResponse({"success": False, "message": "Email and password required."}, status=400)
             result = login_func(email, password)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
             if result:
-=======
-            if result == "SUCCESS":
->>>>>>> Stashed changes
-=======
-            if result == "SUCCESS":
->>>>>>> Stashed changes
-=======
-            if result == "SUCCESS":
->>>>>>> Stashed changes
-                return JsonResponse({"success": True, "message": "Login successful."})
+                u_id, role = result
+                return JsonResponse({"success": True, "message": "Login successful.", "u_id": u_id, "role": role})
             else:
                 return JsonResponse({"success": False, "message": "Invalid credentials."}, status=401)
         except Exception as e:
             return JsonResponse({"success": False, "message": str(e)}, status=500)
     else:
         return JsonResponse({"success": False, "message": "Only POST allowed."}, status=405)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 
 @csrf_exempt
 def user_registration(request):
@@ -81,9 +56,5 @@ def user_registration(request):
             return JsonResponse({"success": False, "message": str(e)}, status=500)
     else:
         return JsonResponse({"success": False, "message": "Only POST allowed."}, status=405)
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+
